347c3e791d406ccc62ad3373db985d468a5d319a
Kenton Shade <kenton@Lenovo-Edge-15-ubuntu-20-04-lts.local>
2021-05-25 15:26:27 -0400

    more testing

--- .github/scripts/identify-os.sh
+++ .github/scripts/identify-os.sh
@@ -5,6 +5,8 @@ uname -a
 ## Our private IP
 PIP=$(hostname -I | awk '{print $1}')
 echo "The private IP is ${PIP}"
+## Test setting env variable from script:
+echo "GHA_ENV_VAR=test_value" >> $GITHUB_ENV
 
 ## Check env variables
 printenv
